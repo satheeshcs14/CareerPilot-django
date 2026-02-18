0c930af962efa9bbe59026cc3a48da5a3b7e0ae8
Satheesh CS <satheeshcs14@gmail.com>
2026-02-18 17:18:35 +0530

    Created jobs app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
-from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('seeker', 'Job Seeker'),
+        ('recruiter', 'Recruiter'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
